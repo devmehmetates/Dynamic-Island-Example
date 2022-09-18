3a04ba055995458ca64fb36a9afb7a0594e2efb5
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-09-18 12:02:05 +0300

    Initial commit

---
+++ Dynamic-Island-Example/Dynamic_Island_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dynamic_Island_ExampleApp.swift
+//  Dynamic-Island-Example
+//
+//  Created by Mehmet Ateş on 18.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Dynamic_Island_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
